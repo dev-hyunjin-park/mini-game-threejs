749fde5b4e0195026a92cb067dbf9ed9b7678196
박현진 <dev.hyunjin.park@gmail.com>
2023-11-12 17:59:40 +0900

    물리 엔진을 적용한 공 만들기

---
+++ src/js/core/Physics.js
@@ -0,0 +1,29 @@
+import * as CANNON from "cannon-es";
+
+export class Physics extends CANNON.World {
+  constructor() {
+    super();
+
+    this.gravity = new CANNON.Vec3(0, -9.82, 0);
+    this.broadphase = new CANNON.SAPBroadphase(this);
+    this.allowSleep = true;
+    // 움직이지 않는 물체는 자동적으로 잠자기 상태로 들어감 -> 물리 계산에서 제외됨 -> 퍼포먼스 향상
+  }
+
+  add(...bodies) {
+    bodies.forEach((body) => this.addBody(body));
+  }
+
+  update(...models) {
+    this.step(1 / 60);
+
+    models.forEach((model) => {
+      if (model.body) {
+        model.position.copy(model.body.position);
+        model.quaternion.copy(model.body.quaternion);
+      }
+    });
+  }
+}
+
+export const SPhysics = new Physics();
--- src/js/scenes/game/Game.js
+++ src/js/scenes/game/Game.js
@@ -1,7 +1,9 @@
 import { SWorld } from "../../core/World";
+import { SPhysics } from "../../core/Physics";
 import * as THREE from "three";
 import { Floor } from "./models/Floor";
 import { Light } from "./tools/Light";
+import { Player } from "./models/Player";
 
 // 월드 클래스를 관리하는 상위 계층의 클래스
 export class Game {
@@ -10,18 +12,23 @@ export class Game {
     this.scene = new THREE.Scene();
     this.world.currentScene = this.scene;
 
+    this.physics = SPhysics;
+
     this.addModels();
   }
 
   addModels() {
+    this.player = new Player(0.3, { x: 0, y: 5, z: 0 });
     this.floor = new Floor(4, 1, 4, { x: 0, y: 0, z: 0 });
     this.light = new Light();
 
-    this.scene.add(this.floor, this.light);
+    this.scene.add(this.player, this.floor, this.light);
+    this.physics.add(this.player.body, this.floor.body);
   }
 
   play() {
     this.world.update(); // 매 프레임마다 update한다
+    this.physics.update(this.player, this.floor);
 
     window.requestAnimationFrame(() => {
       this.play();
