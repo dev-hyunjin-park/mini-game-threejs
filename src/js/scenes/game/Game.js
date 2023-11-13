bfb0f9a2a117d7cf1dd1a7969fa692dac963e276
박현진 <dev.hyunjin.park@gmail.com>
2023-11-13 13:30:02 +0900

    장애물 - Barricade

--- src/js/scenes/game/Game.js
+++ src/js/scenes/game/Game.js
@@ -4,6 +4,7 @@ import * as THREE from "three";
 import { Floor } from "./models/Floor";
 import { Light } from "./tools/Light";
 import { Player } from "./models/Player";
+import { Barricade } from "./models/Barricade";
 
 // 월드 클래스를 관리하는 상위 계층의 클래스
 export class Game {
@@ -18,10 +19,12 @@ export class Game {
   }
 
   addModels() {
-    this.player = new Player(0.3, { x: 0, y: 5, z: 0 });
+    this.player = new Player(0.3, { x: 0, y: 3, z: 9 });
     this.floor1 = new Floor(5, 1, 20, { x: 0, y: 0, z: 0 });
     this.floor2 = new Floor(5, 1, 15, { x: 0, y: 0, z: -20 });
     this.floor3 = new Floor(5, 1, 7, { x: 0, y: 0, z: -35 });
+    this.barricade1 = new Barricade(1.5, 1.5, 0.5, { x: -1.5, y: 1.4, z: 3 });
+    this.barricade2 = new Barricade(1.5, 1.5, 0.5, { x: 2, y: 1.4, z: -2 });
     this.light = new Light();
 
     this.scene.add(
@@ -31,20 +34,31 @@ export class Game {
       this.floor3,
       this.light,
       this.light.target,
-      new THREE.CameraHelper(this.light.shadow.camera)
+      this.barricade1,
+      this.barricade2
+      // new THREE.CameraHelper(this.light.shadow.camera)
     );
     this.physics.add(
       this.player.body,
       this.floor1.body,
       this.floor2.body,
-      this.floor3.body
+      this.floor3.body,
+      this.barricade1.body,
+      this.barricade2.body
     );
   }
 
   play() {
     this.world.update(this.player); // 매 프레임마다 update한다
     this.light.update(this.world.camera);
-    this.physics.update(this.player, this.floor1, this.floor2, this.floor3);
+    this.physics.update(
+      this.player,
+      this.floor1,
+      this.floor2,
+      this.floor3,
+      this.barricade1,
+      this.barricade2
+    );
     window.requestAnimationFrame(() => {
       this.play();
     });
---
+++ src/js/scenes/game/models/Barricade.js
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import gasp from "gsap";
+
+export class Barricade extends THREE.Mesh {
+  name = "barricade";
+  body_ = null;
+
+  get body() {
+    return this.body_;
+  }
+  set body(body) {
+    this.body_ = body;
+  }
+
+  constructor(width, height, depth, position) {
+    const geometry = new THREE.BoxGeometry(width, height, depth);
+    const material = new THREE.MeshBasicMaterial({ color: 0x964b00 });
+
+    super(geometry, material);
+    this.receiveShadow = false;
+    this.castShadow = true;
+    this.body = new PhysicsBarricade(width, height, depth, position);
+  }
+}
+
+export class PhysicsBarricade extends CANNON.Body {
+  name = "barricade";
+  originX = 0;
+
+  constructor(width, height, depth, position) {
+    const duration = Math.random() * 2 + 0.5; // random 속도
+    const shape = new CANNON.Box(
+      new CANNON.Vec3(
+        // 공과 충돌했을 때 공이 파묻히는 현상을 일시적으로 해결하기 위해 +@
+        width / 2 + (0.2 / duration) * (width / 2),
+        height / 2,
+        depth / 2
+      )
+    );
+    const material = new CANNON.Material();
+
+    super({ shape, material, mass: 0, position });
+
+    this.originX = position.x;
+    this.update(duration);
+  }
+
+  update(duration) {
+    gasp.to(this.position, {
+      duration,
+      x: -this.originX, // 바리케이드 초기 x값만큼만 이동
+      ease: "power1.inOut",
+      yoyo: true, // 초기 상태로 돌아옴
+      repeat: -1, // infinite
+    });
+  }
+}
